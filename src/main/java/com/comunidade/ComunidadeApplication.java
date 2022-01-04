91a2226b6b15f3defb9e266d16eecb28353f9978
Rodrigo Mesquita <rodrigo.mesquita@mapinnovation.com.br>
2022-01-04 13:15:03 -0400

    alteracoes

--- src/main/java/com/comunidade/ComunidadeApplication.java
+++ src/main/java/com/comunidade/ComunidadeApplication.java
@@ -2,12 +2,18 @@ package com.comunidade;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ComunidadeApplication {
+public class ComunidadeApplication  extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ComunidadeApplication.class, args);
 	}
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ComunidadeApplication.class);
+    }
 }
