ea8efd4789e4128a46a501c47658a50147efaad4
Rodrigo Mesquita <rodrigo.mesquita@mapinnovation.com.br>
2022-01-13 16:35:45 -0400

    retornando dados do usuário no login

--- src/main/java/com/comunidade/config/SecurityConfig.java
+++ src/main/java/com/comunidade/config/SecurityConfig.java
@@ -79,7 +79,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();	
-		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil,getApplicationContext()));
 		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
